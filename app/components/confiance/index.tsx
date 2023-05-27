35e4140ae3f27f5a491ec497b31aa35fcefd1926
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-05-27 18:55:25 +0100

    update marquee

--- app/components/confiance/index.tsx
+++ app/components/confiance/index.tsx
@@ -1,37 +1,50 @@
+import { useEffect, useState } from "react";
 import { DG, ESC, RISD, WB } from "~/data/images";
 
 export default function ConfianceComponent() {
+
+    let [mySize, setMySize]: any = useState("")
+
+    useEffect(() => {
+        window.addEventListener('resize', changeResize)
+        mySize = window.document.querySelector(".wid");
+    })
+
+    const changeResize = () => {
+        if (window.innerWidth < 900) {
+            setMySize(true)
+        }
+    }
+
     return (
         <div className="bg-white">
-            <div className="py-[7rem]">
+            <div className="pt-[7rem] md:py-[7rem]">
                 <div className="mx-auto">
                     <p className="text-[##0E3758] mt-2 text-center text-lg mx-auto md:text-xl">Ils nous font confiance</p>
                 </div>
                 <div className="container mx-auto">
-                    <marquee>
-                        <div className="scroolpub grid sm:grid-cols-2 md:grid-cols-4 -mx-1 lg:-mx-4 container py-10">
-                            <div className="my-1 px-1 lg:my-4 lg:px-4">
-                                <div className="w-full h-[4rem]">
-                                    <img src={DG} alt="" className="w-full object-cover" />
-                                </div>
-                            </div>
-                            <div className="my-1 px-1 lg:my-4 lg:px-4">
-                                <div className="w-full h-[4rem]">
-                                    <img src={ESC} alt="" className="w-full object-cover" />
-                                </div>
-                            </div>
-                            <div className="my-1 px-1 lg:my-4 lg:px-4">
-                                <div className="w-full h-[4rem]">
-                                    <img src={WB} alt="" className="w-full object-cover" />
-                                </div>
-                            </div>
-                            <div className="my-1 px-1 lg:my-4 lg:px-4">
-                                <div className="w-full h-[4rem]">
-                                    <img src={RISD} alt="" className="w-full object-cover" />
-                                </div>
+                    <div className={`wid wscroolpub grid ${mySize ? 'mymarq' : ''} sm:grid-cols-2 md:grid-cols-4 -mx-1 lg:-mx-4 container py-10`}>
+                        <div className="my-1 px-1 lg:my-4 lg:px-4">
+                            <div className="w-full h-[4rem]">
+                                <img src={DG} alt="" className="w-full object-cover" />
                             </div>
                         </div>
-                    </marquee>
+                        <div className="px-1 my-8 md:my-4 lg:px-4">
+                            <div className="w-full h-[4rem]">
+                                <img src={ESC} alt="" className="w-full object-cover" />
+                            </div>
+                        </div>
+                        <div className="px-1 my-8 md:my-4 lg:px-4">
+                            <div className="w-full h-[4rem]">
+                                <img src={WB} alt="" className="w-full object-cover" />
+                            </div>
+                        </div>
+                        <div className="px-1 my-8 md:my-4 lg:px-4">
+                            <div className="w-full h-[4rem]">
+                                <img src={RISD} alt="" className="w-full object-cover" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
