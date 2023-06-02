17603da2035e28bfc0b4ea856764d6e1d958a401
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-06-02 07:34:49 +0100

    update teams

--- app/components/equipes/index.tsx
+++ app/components/equipes/index.tsx
@@ -1,81 +1,74 @@
-import { useState } from "react";
-import { equipes } from "~/data/equipes";
-import BtnNext from "../../../assets/imgs/next1.png"
-import BtnPrevious from "../../../assets/imgs/previous1.png"
-import type { EquipeType } from "~/types";
+import { useState } from "react"
+import { equipes } from "~/data/equipes"
+import nameImg from "../../../assets/imgs/name.png"
+import jobImg from "../../../assets/imgs/job.png"
+import mailImg from "../../../assets/imgs/mail.png"
+import type { EquipeType } from "~/types"
+import { Modal } from "../commons/modal"
 
 export default function EquipesComponent() {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const next = () => {
-        setCurrentIndex((currentIndex + 1) % equipes.length);
-    };
-    const prev = () => {
-        setCurrentIndex((currentIndex - 1 + equipes.length) % equipes.length);
-    };
+    const [detail, setDetail] = useState(false)
+    const [myIndex, setMyIndex] = useState(0)
     return (
         <div className="bg-[#f6fafe]">
             <div className="py-16 md:py-44">
                 <div className="container mx-auto">
-                    <div className="flex">
+                    <div className="flex flex-row">
                         <div className="w-full">
-                            <p className="text-secondary mt-2 text-lg md:text-xl">Notre équipe </p>
+                            <p className="text-secondary mt-2 text-lg md:text-xl">Notre équipe</p>
                             <h1
                                 className="text-primary mt-1 text-xl  md:leading-[120%] md:text-[2rem]  md:mr-8  md:w-[40rem] text-start">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             </h1>
                             <div className="pt-[3rem]">
-                                <div className="flex flex-row justify-center items-center">
-                                    <button onClick={prev} className="w-8 h-8 hidden md:flex">
-                                        <img src={BtnPrevious} alt="" />
-                                    </button>
-                                    <div className="md:w-11/12 flex items-center justify-center">
-                                        {equipes.map((item: EquipeType, index) => (
-                                            <div
-                                                key={index}
-                                                className={
-                                                    equipes[currentIndex].name === item.name ? 'fade' : 'slide fade'
-                                                }
-                                            >
-                                                <div className="flex mx-2 md:mx-8 flex-col md:flex-row">
-                                                    <div className="w-36 md:w-52 bg-blue-400 rounded-full h-36 md:h-52 md:mx-4"></div>
-                                                    <div className="md:ml-4 md:w-9/12 flex flex-col md:justify-start">
-                                                        <h1
-                                                            className="text-2xl md:text-3xl text-start mt-4 md:mt-0">
-                                                            {item.name}
-                                                        </h1>
-                                                        <h4
-                                                            className="text-base text-start font-medium">
-                                                            {item.title}
-                                                        </h4>
-                                                        <p className="text-xs mt-4 text-start leading-5">
-                                                            {item.description}
-                                                        </p>
-                                                        <hr className="text-gray-400 my-2" />
-                                                        <p className="text-xs mt-2 md:mt-0 text-start">
-                                                            {item.mail}
-                                                        </p>
+                                <div className="grid md:grid-cols-3 grid-cols-1 space-y-4 md:space-y-0">
+                                    {
+                                        equipes.map((item: EquipeType, index) => {
+                                            return (
+                                                <div onClick={() => {
+                                                    setMyIndex(index)
+                                                    setDetail(true)
+                                                }} key={index} className="h-32 md:mb-6 md:w-11/12 ">
+                                                    <div className="p-4 border border-gray-400 hover:border-2 cursor-pointer flex items-start flex-col justify-center rounded-md h-full">
+                                                        <h2
+                                                            className="md:w-[14rem] xl:w-[18rem] text-[1.2rem] text-primary">{item.name}</h2>
+                                                        <h2
+                                                            className="text-sm mt-2 font-normal leading-[130%] text-primary">{item.title}</h2>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                    <button onClick={next} className="w-8 h-8 hidden md:flex">
-                                        <img src={BtnNext} alt="" />
-                                    </button>
+                                            )
+                                        })
+                                    }
                                 </div>
-                            </div>
-                            <div className="flex justify-start items-center mt-2 md:hidden">
-                                <button onClick={prev} className="w-6 h-6 m-1">
-                                    <img src={BtnPrevious} alt="" />
-                                </button>
-                                <button onClick={next} className="w-6 h-6 ml-1">
-                                    <img src={BtnNext} alt="" />
-                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            {
+                detail && (
+                    <Modal
+                        onClose={() => setDetail(false)}
+                        title=""
+                    >
+                        <div className="px-4">
+                            <div className="flex flex-row items-center">
+                                <img src={nameImg} className="w-8 h-8" alt="" />
+                                <div>
+                                    <h1 className='text-2xl py-1 font-semibold ml-4'>{equipes[myIndex].name}</h1>
+                                    <p className='text-xs font-normal ml-4'>{equipes[myIndex].title}</p>
+                                </div>
+                            </div>
+                            <hr className="my-2" />
+                            <p className='text-xs leading-6 py-1'>{equipes[myIndex].description}</p>
+                            <hr className="my-2" />
+                            <a href={`mailto:${equipes[myIndex].mail}`} className="text-xs font-normal cursor-pointer pt-4 hover:underline hover:underline-offset-4">{equipes[myIndex].mail}</a>
+                        </div>
+                    </Modal>
+                )
+
+            }
         </div>
+
     )
 }
