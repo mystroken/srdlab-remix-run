31ffa6e2baf5d9832b5b578c5ffc99d5daedd06e
Abba Sali <abbasaliaboubakar@gmail.com>
2023-08-18 12:41:08 +0100

    add button name

--- app/components/commons/button.tsx
+++ app/components/commons/button.tsx
@@ -4,5 +4,5 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button = ({ name, click, ...props }: ButtonProps) => {
-    return <button className="bg-orange rounded-full text-[.9rem] text-primary px-[30px] py-[10px] md:py-[16px] md:px-[48px] w-full font-semibold">{name}</button>
+    return <button aria-label="search" className="bg-orange rounded-full text-[.9rem] text-primary px-[30px] py-[10px] md:py-[16px] md:px-[48px] w-full font-semibold">{name}</button>
 } 
