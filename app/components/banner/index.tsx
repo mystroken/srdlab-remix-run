b6fc28893827d802dbc00c60089eb6ad97005571
Abba Sali <abbasaliaboubakar@gmail.com>
2023-08-28 21:24:39 +0100

    simplify

--- app/components/banner/index.tsx
+++ app/components/banner/index.tsx
@@ -3,13 +3,13 @@ import LogoColor from "../../../assets/imgs/logo.png"
 import BtnNext from "../../../assets/imgs/next.png"
 import BtnPrevious from "../../../assets/imgs/previous.png"
 import styles from "./styles.css"
-import { useEffect, useState } from "react"
-import { Link, useMatches } from "@remix-run/react"
-import { carouselItems, header } from "~/data/header"
-import type { HeaderType, SolutionsType } from "~/types"
+import {useEffect, useState} from "react"
+import {Link, useMatches} from "@remix-run/react"
+import {carouselItems, header} from "~/data/header"
+import type {HeaderType, SolutionsType} from "~/types"
 
 export function links() {
-    return [{ rel: "stylesheet", href: styles }]
+    return [{rel: "stylesheet", href: styles}]
 }
 
 interface BannerComponentProps {
@@ -20,7 +20,7 @@ interface BannerComponentProps {
 }
 
 
-export default function BannerComponent({ title, content, arrow, list, ...props }: BannerComponentProps) {
+export default function BannerComponent({title, content, arrow, list}: BannerComponentProps) {
     const [navbar, setNavbar] = useState(false);
     const [isShow, setIsShow] = useState(false)
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -31,14 +31,13 @@ export default function BannerComponent({ title, content, arrow, list, ...props 
     })
 
     const handleScrollToTop = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' })
+        window.scrollTo({top: 0, behavior: 'smooth'})
     }
 
     const changeNavbarColor = () => {
         if (window.scrollY >= 80) {
             setColorchange(true);
-        }
-        else {
+        } else {
             setColorchange(false);
         }
     };
@@ -60,28 +59,31 @@ export default function BannerComponent({ title, content, arrow, list, ...props 
                 <div className={`fixed right-0 top-0 left-0 z-40 ${colorChange ? 'bg-primary' : ''}`}>
 
                     <div className="myheader container-other hover:rounded-xl my-4"
-                        onMouseLeave={() => setIsShow(false)}
-                        onMouseEnter={() => setIsShow(true)}
+                         onMouseLeave={() => setIsShow(false)}
+                         onMouseEnter={() => setIsShow(true)}
                     >
                         <div className="py-2 container mx-auto">
                             <nav>
                                 <div className="flex items-center justify-between">
                                     <Link to="/" onClick={handleScrollToTop}>
                                         {
-                                            isShow === true ?
+                                            isShow ?
                                                 (<div>
-                                                    <img className={`logopng w-[13rem] h-[4.4rem] logocolor`} src={LogoColor} alt="" />
+                                                    <img className={`logopng w-[13rem] h-[4.4rem] logocolor`}
+                                                         src={LogoColor} alt=""/>
                                                 </div>) : (
                                                     <div>
-                                                        <img className="logosvg w-[13rem] h-[4.4rem] logowhite" src={Logo} alt="" />
+                                                        <img className="logosvg w-[13rem] h-[4.4rem] logowhite" src={Logo}
+                                                             alt=""/>
                                                     </div>
                                                 )
                                         }
                                     </Link>
                                     <div className="xl:hidden" onClick={() => setNavbar(!navbar)}>
                                         <button>
-                                            <svg className="h-8 w-8 fill-current text-white" fill="none" strokeLinecap="round" strokeLinejoin="round"
-                                                strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                                            <svg className="h-8 w-8 fill-current text-white" fill="none"
+                                                 strokeLinecap="round" strokeLinejoin="round"
+                                                 strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path d="M4 6h16M4 12h16M4 18h16"></path>
                                             </svg>
                                         </button>
@@ -92,7 +94,8 @@ export default function BannerComponent({ title, content, arrow, list, ...props 
                                                 header.map((item: HeaderType, index) => {
                                                     return (
                                                         <li onClick={handleScrollToTop} key={index}>
-                                                            <Link to={`${item.link}`} className={`${item.link === idPath ? 'font-extrabold border rounded-full px-6 py-2 border-[#faaf42]' : 'font-normal'} text-sm ${item.link === '#travailleravecnous' ? '' : ''}`}>{item.name}</Link>
+                                                            <Link to={`${item.link}`}
+                                                                  className={`${item.link === idPath ? 'font-extrabold border rounded-full px-6 py-2 border-[#faaf42]' : 'font-normal'} text-sm ${item.link === '#travailleravecnous' ? '' : ''}`}>{item.name}</Link>
                                                         </li>
                                                     )
                                                 })
@@ -102,14 +105,16 @@ export default function BannerComponent({ title, content, arrow, list, ...props 
                                 </div>
                                 <div
                                     className={`flex-1 justify-self-center pb-3 mt-8 lg:block md:pb-0 md:mt-0 ${navbar ? 'block' : 'hidden'
-                                        }`}
+                                    }`}
                                 >
                                     <ul className="flex flex-col items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 lg:hidden text-white">
                                         {
                                             header.map((item: HeaderType, index) => {
                                                 return (
                                                     <li onClick={handleScrollToTop} key={index}>
-                                                        <Link className="md:text-justify text-gray-800 font-medium text-sm mt-4" to={`${item.link}`}>{item.name}</Link>
+                                                        <Link
+                                                            className="md:text-justify text-gray-800 font-medium text-sm mt-4"
+                                                            to={`${item.link}`}>{item.name}</Link>
                                                     </li>
                                                 )
                                             })
@@ -120,14 +125,14 @@ export default function BannerComponent({ title, content, arrow, list, ...props 
                         </div>
                     </div>
                 </div>
-            </header >
+            </header>
             {
                 list === true ? (
                     <header className="bg-center  bg-cover z-40 pt-[6.5rem] md:h-[600px]">
                         <div className="py-4 container mx-auto sticky top-0 z-20">
-                            <div className="mt-[4.4rem] md:mt-[8.8rem] md:w-[44rem]">
+                            <div className="mt-[4.4rem] md:mt-[8.8rem] md:w-[44rem] md:h-[28rem]">
                                 <>
-                                    <div className='slider-container'>
+                                    <div className='slider-container md:h-64'>
                                         {carouselItems.map((item: SolutionsType, index) => (
                                             <div
                                                 key={index}
@@ -136,23 +141,29 @@ export default function BannerComponent({ title, content, arrow, list, ...props 
                                                 }
                                             >
                                                 <h1
-                                                    className="md:text-justify text-white text-2xl text-center md:text-[2.5rem] md:mr-8 leading-[2rem] md:leading-[120%]">
+                                                    className="md:text-start text-white text-2xl text-center md:text-[2.5rem] md:mr-8 leading-[2rem] md:leading-[120%]">
                                                     {item.title}
                                                 </h1>
-                                                <hr className="h-1 mt-10 bg-orange w-44 mx-auto md:mx-0" /> : <div className="my-8" />
-                                                <p className="md:text-justify text-white text-lg md:text-[1.25rem] md:w-3/4 text-center leading-[1.7rem]">
+                                                <hr className="h-1 mt-10 bg-orange w-44 mx-auto md:mx-0"/>
+                                                : <div className="my-8"/>
+                                                <p className="md:text-start text-white text-[1.125rem] md:text-[1.25rem] md:w-3/4 text-center leading-[2rem]">
                                                     {item.content}
                                                 </p>
                                             </div>
                                         ))}
-                                        <div className="flex mt-6 md:mt-0 justify-center md:justify-end">
-                                            <button onClick={prev} className="w-6 h-6 mx-2">
-                                                <img src={BtnPrevious} alt="" />
-                                            </button>
-                                            <button onClick={next} className="w-6 h-6 mx-2">
-                                                <img src={BtnNext} alt="" />
-                                            </button>
-                                        </div>
+                                    </div>
+                                    <div className="hidden md:flex md:absolute md:right-0 md:top-0">
+                                        <p className={'text-[24px] w-[46px] text-gray-400'}>{currentIndex+1}/{carouselItems.length}</p>
+                                    </div>
+                                    <div className="flex justify-center md:absolute md:right-0 md:bottom-40">
+                                        <button onClick={prev}
+                                                className="w-10 h-10 mx-2 flex justify-center items-center border border-gray-400 rounded-full p-1">
+                                            <img src={BtnPrevious} alt=""/>
+                                        </button>
+                                        <button onClick={next}
+                                                className="w-10 h-10 mx-2 flex justify-center items-center border border-gray-400 rounded-full p-1">
+                                            <img src={BtnNext} alt=""/>
+                                        </button>
                                     </div>
                                 </>
                             </div>
@@ -167,7 +178,7 @@ export default function BannerComponent({ title, content, arrow, list, ...props 
                                     {title}
                                 </h1>
                                 {arrow === true ?
-                                    <hr className="my-8 h-1 bg-orange w-44 mx-auto md:mx-0" /> : <div className="my-8" />
+                                    <hr className="my-8 h-1 bg-orange w-44 mx-auto md:mx-0"/> : <div className="my-8"/>
                                 }
                                 <p className="text-white text-lg md:text-[1.25rem] md:w-3/4 text-center md:text-start leading-[1.7rem]">
                                     {content}
@@ -175,19 +186,19 @@ export default function BannerComponent({ title, content, arrow, list, ...props 
                                 {arrow === true ?
                                     <div className="flex justify-center md:justify-end mt-16">
                                         <button onClick={prev} className="w-6 h-6 mx-2">
-                                            <img src={BtnPrevious} alt="" />
+                                            <img src={BtnPrevious} alt=""/>
                                         </button>
                                         <button onClick={next} className="w-6 h-6 mx-2">
-                                            <img src={BtnNext} alt="" />
+                                            <img src={BtnNext} alt=""/>
                                         </button>
                                     </div>
-                                    : <div />
+                                    : <div/>
                                 }
                             </div>
                         </div>
                     </header>
                 )
             }
-        </div >
+        </div>
     )
 }
