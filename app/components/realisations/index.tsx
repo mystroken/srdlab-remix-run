c66c3b48924f8c799d0a7cdfc8e68ea921a94550
Abba Sali <abbasaliaboubakar@gmail.com>
2023-09-16 15:52:49 +0100

    update realisations

--- app/components/realisations/index.tsx
+++ app/components/realisations/index.tsx
@@ -1,52 +1,43 @@
-import { PosteType } from "~/types";
-import { cleanText } from "~/utils/helpers";
+import React from "react";
+import {cleanText} from "~/utils/helpers";
 
 export default function Realisations({
-  slug,
-  date,
-  context,
-  open,
-  name,
-  title,
-}: PosteType) {
-  return (
-    <a
-      //href={`realisations/${slug}`}
-      href={"#realisations"}
-      className={`my-1 lg:my-4 border hover:shadow-xl cursor-pointer`}
-    >
-      <article className="rounded-lg mb-4 md:mb-0">
-        <div className="p-4">
-          <div className={"flex justify-between items-center py-3"}>
-            <p className="text-primary text-sm leading-[155%] font-semibold">
-              {cleanText(name, 60)}
-            </p>
-            <div
-              className={`rounded-full text-xs w-16 text-white px-4 py-2 ${
-                open === true ? "bg-green-500" : "bg-red-500"
-              } h-6 flex justify-center items-center`}
-            >
-              {open === true ? "en_cours" : "achevée"}
-            </div>
-          </div>
-          <p className="text-primary text-lg leading-[155%] font-semibold">
-            {cleanText(title, 60)}
-          </p>
+                                         slug,
+                                         date,
+                                         context,
+                                         open,
+                                         name,
+                                         title,
+                                         picture
+                                     }: any) {
+    return (
+        <div
+            className={`my-1 lg:my-4`}>
+            <article className="rounded-lg mb-4 md:mb-0">
+                <a href={`/realisations/${slug}`} className="relative block w-full bg-gray-900 group">
+                    <img
+                        className="absolute inset-0 object-cover border-gray-500 w-full h-full group-hover:opacity-20 border"
+                        src={picture}/>
+                    <div className="relative p-5">
+                        <div className="mt-40">
+                            <div
+                                className="transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
+                                <div className="p-2">
+                                    <p className="text-gray-200 text-lg leading-[155%] font-semibold">
+                                        {cleanText(name, 40)}
+                                    </p>
+                                    <div
+                                        className={`bg-orange text-white rounded-full text-xs px-4 py-2 text-center cursor-pointer mt-4`}>
+                                        Visiter
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </article>
         </div>
-        <hr />
-        <div className={"p-4"}>
-          <div className="flex items-center my-auto mt-2">
-            <p className="text-[.8rem] leading-[170%] underline outline-offset-4 md:text-justify">
-              Date de realisation:
-            </p>
-            <p className="pl-2 text-[.8rem] leading-[170%]">{date}</p>
-          </div>
-          <p className="mt-4 text-[.8rem] leading-[170%]">
-            {cleanText(context, 200)}
-          </p>
-        </div>
-      </article>
-    </a>
-  );
+    );
 }
-//
+
--- app/routes/realisations/index.tsx
+++ app/routes/realisations/index.tsx
@@ -1,137 +1,145 @@
 import BannerComponent from "~/components/banner";
-import { DefaultLayout } from "~/layouts/default";
-import React, { useState } from "react";
-import { realisations } from "~/data/realisations";
-import { PosteType } from "~/types";
+import {DefaultLayout} from "~/layouts/default";
+import React, {useState} from "react";
+import {realisations} from "~/data/realisations";
 import Realisations from "~/components/realisations";
-import { BUT } from "~/data/images";
+import {BUT} from "~/data/images";
 
 export default function NosRessourcesRoute() {
-  const [openAll, setOpenAll] = useState(true);
-  const [openRealise, setOpenRealise] = useState(false);
-  const [openEnCours, setOpenEnCours] = useState(false);
+    const [openAll, setOpenAll] = useState(true);
+    const [openRealise, setOpenRealise] = useState(false);
+    const [openEnCours, setOpenEnCours] = useState(false);
 
-  const selectRealise = () => {
-    setOpenAll(false);
-    setOpenEnCours(false);
-    setOpenRealise(true);
-  };
+    const selectRealise = () => {
+        setOpenAll(false);
+        setOpenEnCours(false);
+        setOpenRealise(true);
+    };
 
-  const selectEnCours = () => {
-    setOpenAll(false);
-    setOpenEnCours(true);
-    setOpenRealise(false);
-  };
+    const selectEnCours = () => {
+        setOpenAll(false);
+        setOpenEnCours(true);
+        setOpenRealise(false);
+    };
 
-  const selectAll = () => {
-    setOpenAll(true);
-    setOpenEnCours(false);
-    setOpenRealise(false);
-  };
-  return (
-    <>
-      <DefaultLayout>
-        <BannerComponent
-          title="Nos realisations"
-          content="Découvrez nos réalisations achevées et celles qui sont en cours."
-        />
-        <div className="container mx-auto">
-          <div className="flex justify-center items-center my-10">
-            <img className="w-full md:w-60 h-auto" loading="lazy" src={BUT} />
-          </div>
-          <div className="py-10">
-            <div
-              className={
-                "flex md:mt-0 flex-row justify-center items-center space-x-8 text-sm font-normal"
-              }
-            >
-              <div
-                onClick={selectAll}
-                className={`cursor-pointer ${
-                  openAll ? "bg-orange rounded-full text-xs px-4 py-2" : ""
-                }`}
-              >
-                TOUT
-              </div>
-              <div
-                className={`${
-                  openRealise
-                    ? "bg-green-500 text-white rounded-full text-xs px-4 py-2"
-                    : ""
-                } text-center cursor-pointer`}
-                onClick={selectRealise}
-              >
-                EN COURS
-              </div>
-              <div
-                className={`${
-                  openEnCours
-                    ? "bg-red-500 rounded-full text-xs text-white px-4 py-2"
-                    : ""
-                } cursor-pointer`}
-                onClick={selectEnCours}
-              >
-                ACHEVEES
-              </div>
-            </div>
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 container pt-10 gap-y-4  md:gap-x-10">
-              {openAll &&
-                realisations.map((item: PosteType, index: number) => {
-                  return (
-                    <Realisations
-                      key={index}
-                      open={item.open}
-                      slug={item.slug}
-                      name={item.name}
-                      title={item.title}
-                      date={item.date}
-                      role={item.role}
-                      context={item.context}
-                      description={item.description}
-                    />
-                  );
-                })}
-              {openRealise &&
-                realisations.map((item: PosteType, index: number) => {
-                  while (item.open === openRealise) {
-                    return (
-                      <Realisations
-                        key={index}
-                        open={item.open}
-                        slug={item.slug}
-                        name={item.name}
-                        title={item.title}
-                        date={item.date}
-                        role={item.role}
-                        context={item.context}
-                        description={item.description}
-                      />
-                    );
-                  }
-                })}
-              {openEnCours &&
-                realisations.map((item: PosteType, index: number) => {
-                  while (item.open === false) {
-                    return (
-                      <Realisations
-                        key={index}
-                        open={item.open}
-                        slug={item.slug}
-                        name={item.name}
-                        title={item.title}
-                        date={item.date}
-                        role={item.role}
-                        context={item.context}
-                        description={item.description}
-                      />
-                    );
-                  }
-                })}
-            </div>
-          </div>
-        </div>
-      </DefaultLayout>
-    </>
-  );
+    const selectAll = () => {
+        setOpenAll(true);
+        setOpenEnCours(false);
+        setOpenRealise(false);
+    };
+    return (
+        <>
+            <DefaultLayout>
+                <BannerComponent
+                    title="Nos realisations"
+                    content="Découvrez nos réalisations achevées et celles qui sont en cours."
+                />
+                <div className="container mx-auto">
+                    <div className="flex justify-center items-center my-10">
+                        <img className="w-full md:w-60 h-auto" loading="lazy" src={BUT}/>
+                    </div>
+                    <div className="py-10">
+                        <div
+                            className={
+                                "flex md:mt-0 flex-row justify-center items-center space-x-8 text-sm font-normal"
+                            }
+                        >
+                            <div
+                                onClick={selectAll}
+                                className={`cursor-pointer ${
+                                    openAll ? "bg-orange rounded-full text-xs px-4 py-2" : ""
+                                }`}
+                            >
+                                TOUT
+                            </div>
+                            <div
+                                className={`${
+                                    openRealise
+                                        ? "bg-green-500 text-white rounded-full text-xs px-4 py-2"
+                                        : ""
+                                } text-center cursor-pointer`}
+                                onClick={selectRealise}
+                            >
+                                EN COURS
+                            </div>
+                            <div
+                                className={`${
+                                    openEnCours
+                                        ? "bg-red-500 rounded-full text-xs text-white px-4 py-2"
+                                        : ""
+                                } cursor-pointer`}
+                                onClick={selectEnCours}
+                            >
+                                ACHEVEES
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-center mt-12">
+
+
+                        </div>
+
+                        <div className="grid sm:grid-cols-2 md:grid-cols-3 container pt-10 gap-y-4  md:gap-x-10">
+                            {openAll &&
+                                realisations.map((item: any, index: number) => {
+                                    return (
+                                        <Realisations
+                                            key={index}
+                                            open={item.open}
+                                            slug={item.slug}
+                                            name={item.name}
+                                            title={item.title}
+                                            date={item.date}
+                                            picture={item.picture}
+                                            role={item.role}
+                                            context={item.context}
+                                            description={item.description}
+                                        />
+                                    );
+                                })}
+                            {openRealise &&
+                                realisations.map((item: any, index: number) => {
+                                    while (item.open === openRealise) {
+                                        return (
+                                            <Realisations
+                                                key={index}
+                                                open={item.open}
+                                                slug={item.slug}
+                                                name={item.name}
+                                                title={item.title}
+                                                date={item.date}
+                                                role={item.role}
+                                                picture={item.picture}
+                                                context={item.context}
+                                                description={item.description}
+                                            />
+                                        );
+                                    }
+                                })}
+                            {openEnCours &&
+                                realisations.map((item: any, index: number) => {
+                                    while (item.open === false) {
+                                        return (
+                                            <Realisations
+                                                key={index}
+                                                open={item.open}
+                                                slug={item.slug}
+                                                name={item.name}
+                                                title={item.title}
+                                                date={item.date}
+                                                picture={item.picture}
+                                                role={item.role}
+                                                context={item.context}
+                                                description={item.description}
+                                            />
+                                        );
+                                    }
+                                })}
+                        </div>
+                    </div>
+                </div>
+            </DefaultLayout>
+        </>
+    );
 }
 
