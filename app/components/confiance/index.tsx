3dcb4a6ed9066905f4a60ee715bf610f2802598d
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-11-21 08:45:55 +0100

    add lib marquee

--- app/components/confiance/index.tsx
+++ app/components/confiance/index.tsx
@@ -1,41 +1,40 @@
-import {confiances} from "~/data/confiances";
-import {Image} from "remix-image";
-import {useTranslation} from "react-i18next";
+import { confiances } from "~/data/confiances";
+import { Image } from "remix-image";
+import { useTranslation } from "react-i18next";
+import Marquee from "react-fast-marquee";
 
 export default function ConfianceComponent() {
-    let {t} = useTranslation();
+  let { t } = useTranslation();
 
-    return (
-        <div className="bg-white">
-            <div className="pt-[5rem]">
-                <div className="mx-auto">
-                    <p className="text-[##0E3758] mt-2 text-center text-lg mx-auto md:text-xl">
-                        {t("confiance")}
-                    </p>
-                </div>
-                <div className="container mx-auto md:overflow-x-hidden">
-                    <div className={"md:animate-marquee-infinite"}>
-                        <div
-                            className={`wscroolpub grid gap-y-12 xl:gap-y-0  sm:grid-cols-4 xl:grid-cols-5 -mx-1 lg:-mx-4 container py-10`}
-                        >
-                            {confiances.map((item, index) => {
-                                return (
-                                    <div key={index} className="my-1 px-1 lg:my-4 lg:px-4">
-                                        <div className="w-full h-[4rem]">
-                                            <Image
-                                                src={item}
-                                                loading="lazy"
-                                                alt="pic"
-                                                className="w-full object-cover h-auto"
-                                            />
-                                        </div>
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="bg-white">
+      <div className="pt-[5rem]">
+        <div className="mx-auto">
+          <p className="text-[#0E3758] mt-2 text-center text-lg mx-auto md:text-xl">
+            {t("confiance")}
+          </p>
         </div>
-    );
+        <div className="container mx-auto">
+          <div className={`py-10`}>
+            <Marquee>
+              {confiances.map((item, index) => {
+                return (
+                  <div key={index} className="my-1 px-1 lg:my-4 lg:px-4">
+                    <div className="w-full">
+                      <Image
+                        src={item}
+                        loading="lazy"
+                        alt="pic"
+                        className="w-[12rem] object-cover h-auto"
+                      />
+                    </div>
+                  </div>
+                );
+              })}
+            </Marquee>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
