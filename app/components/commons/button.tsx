ca1dad5dfcf4afcd95cec433dfa297660c3f697d
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-12-05 14:25:57 +0100

    refactoring: button component

--- app/components/commons/button.tsx
+++ app/components/commons/button.tsx
@@ -1,16 +1,19 @@
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+import React, { ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   name: string;
-  click?: () => Function;
+  click?: () => void;
 }
 
-export const Button = ({ name, click, ...props }: ButtonProps) => {
-  return (
-    <button
-      aria-label="search"
-      className="bg-orange rounded-full text-[.9rem] text-primary px-[30px] py-[10px] md:py-[16px] md:px-[48px] w-full font-semibold"
-    >
-      {name}
-    </button>
-  );
-};
+const Button: React.FC<ButtonProps> = ({ name, click, ...props }) => (
+  <button
+    aria-label="search"
+    className="bg-orange rounded-full text-[.9rem] text-primary px-[30px] py-[10px] md:py-[16px] md:px-[48px] w-full font-semibold"
+    onClick={click}
+    {...props}
+  >
+    {name}
+  </button>
+);
 
+export default Button;
