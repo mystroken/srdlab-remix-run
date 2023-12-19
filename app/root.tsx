eb2992b99c94a73f120424862c8fad9b91e945d9
Abba Sali Aboubakar Mamate <abbasaliaboubakar@gmail.com>
2023-12-19 05:39:42 +0100

    update spinner timer

--- app/root.tsx
+++ app/root.tsx
@@ -49,7 +49,7 @@ export default function App() {
   useEffect(() => {
     const delay = setTimeout(() => {
       setIsLoading(false);
-    }, 2000);
+    }, 1000);
     return () => clearTimeout(delay);
   }, []);
   useChangeLanguage(locale);
